78434c31279172e7409d4868ddcefd671a2ead28
Taranjot Singh <taranjotscience@gmail.com>
2025-08-03 01:16:44 -0300

    Updated with working status

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const morgan = require('morgan')
+app.set('view engine', 'ejs');
+app.listen(3000);
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
 app.get('/blogs', (req, res) => {
-    res.send("Hello world!");
+
+    const blogs = [
+        {title: 'Isaac Newton', snippet: 'An apple fell on my head'},
+        {title: 'Paul Dirac', snippet: 'Dirac Equation is more than it looks'},
+        {title: 'Richard P. Feynman', snippet: 'Differentiate under the integral sign'}
+    ]
+
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/', (req, res) => {
-    res.send('Oops!, page not found (404)')
+    res.redirect('/blogs');
+
 })
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create'});
 })
 
+app.use((req,res) => {
+    res.status(404).render('404', {title: '404'});
+})
