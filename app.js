5acc74c1c57134d9d41f7cd02151ce86fa73a8ec
Taranjot Singh <taranjotscience@gmail.com>
2025-08-05 11:16:06 -0300

    update with get, post, delete

--- app.js
+++ app.js
@@ -17,12 +17,14 @@ app.set('view engine', 'ejs');
 //middleware
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
 
     res.redirect('/blogs');
 });
+
 
 app.get('/blogs', (req, res) => {
     Blog.find().sort({createdAt: -1})
@@ -38,6 +40,31 @@ app.get('/blogs', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 });
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', {blog: result, title: 'Blog Details'});
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create'});
